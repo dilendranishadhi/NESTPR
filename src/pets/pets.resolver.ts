facc5fd8ce71a4e7d0e30f49c3d4917cc669c0a5
dilendran <96422515+dilendran@users.noreply.github.com>
2022-02-09 16:56:39 +0530

    initial

---
+++ src/pets/pets.resolver.ts
@@ -0,0 +1,27 @@
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreatePetInput } from './dto/create-pet.input';
+import { Pet } from './pet.entity';
+import { PetsService } from './pets.service';
+
+@Resolver(of => Pet)
+export class PetsResolver {
+
+    constructor(private petService: PetsService) {}
+
+    @Query(returns =>[Pet])
+    pets(): Promise<Pet[]> {
+      return this.petService.findAll();
+    }
+    
+  @Mutation(returns => Pet)
+  createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<Pet> {
+    return this.petService.createPet(createPetInput);
+  }
+  @Query(returns => Pet)
+  getPet(@Args('id', { type: () => Int }) id: number): Promise<Pet> {
+    return this.petService.findOne(id);
+  }
+  
+}
